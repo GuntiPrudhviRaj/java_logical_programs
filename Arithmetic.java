8fbeaf1d4ac129e98dd08ce2259c16349fcb34b0
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-18 13:11:42 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,12 @@
+class Arithmetic
+  {
+    public static void main(String args[])
+    {
+      int a=40,b=30;
+        System.out.println("The Addition of "+a+"and"+b+"is:"+(a+b));
+        System.out.println("The Subtraction of "+a+"and"+b+"is:"+(a-b));
+          System.out.println("The multiplication of "+a+"and"+b+"is:"+(a*b));
+          System.out.println("The division of "+a+"and"+b+"is:"+(a/b) );
+          System.out.println("The modulus of "+a+"and"+b+"is:"+(a%b));
+    }
+  }
