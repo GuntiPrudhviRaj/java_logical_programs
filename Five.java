456cbe021768b4c5dc9856a5837435756a4da246
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-18 13:10:38 +0530

    Add files via upload

---
+++ Five.java
@@ -0,0 +1,11 @@
+class Five
+  {
+    public static void main(String args[])
+    {
+      String st="prudhvi";
+      String st1=new String("Hello");
+
+      System.out.println("The Given name is:"+st);
+      System.out.println("The Given String is:"+st1);
+    }
+  }
