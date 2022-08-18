46ea8eb06d8b4f4fb48add703483a41549d865e1
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-18 13:12:08 +0530

    Add files via upload

---
+++ Radius.java
@@ -0,0 +1,11 @@
+class Radius
+  {
+    public static void main(String args[])
+    {
+      int r=6;
+      float pi=3.14F;
+      System.out.println("The area of the circle radius "+r+"and area is:"+(pi*r*r));
+        
+        
+    }
+  }
