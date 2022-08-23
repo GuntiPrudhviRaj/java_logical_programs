13fe3521287dc77b78e840024a33c049d06887a4
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-23 12:29:06 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Even
+  {
+    public static void main(String args[])
+    {
+      int num;
+      Scanner sc= new Scanner(System.in);
+      System.out.println("enter num value:");
+      num=sc.nextInt();
+      if(num%2==0)
+      {
+        System.out.println("print num is even:");
+      }
+      else
+      {
+        System.out.println("print num is not even:");
+      } 
+      
+    }
+  }
