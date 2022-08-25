5028963a314d91f143e51f9aff9f79adab5f2d0a
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-25 16:28:25 +0530

    Add files via upload

---
+++ Weird (1).java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Weird
+  {
+    public static void main(String args[])
+    {
+      int n;
+      Scanner sc = new Scanner(System.in);
+      System.out.println("enter the number:");
+      n=sc.nextInt();
+      if(n%2!=0)
+      { 
+        System.out.println("weird");
+      }  
+      else if(n>=2&&n<=5)
+      {  
+        System.out.println("not weird");
+      }  
+      else if(n>=6&&n<=20)
+      {  
+        System.out.println("weird");
+      }  
+      else
+      {
+        System.out.println("not weird");
+      }  
+      
+    }
+  }
