755c26dac7d13b5f37b88a175af5483e26c0f5b4
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-30 10:26:14 +0530

    Add files via upload

---
+++ Table (2).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Table
+  {
+    public static void main(String args[])
+    { 
+      int i,n;
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter n value");
+       n=sc.nextInt();
+
+      for(i=1;i<=10;i++)
+        {
+          System.out.println(n+"x"+i+"="+n*+i);
+          
+        }
+    }
+  }
