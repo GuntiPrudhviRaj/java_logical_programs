c163cdbeec8031250d26bd0e807a9b7988c052be
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-08-30 10:24:42 +0530

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,12 @@
+class Loop
+  {
+   public static void main(String args[])
+     {
+       int i;
+
+      for(i=10;i>=1;i--)
+        {
+          System.out.println("print i vaue:"+i);
+        }
+     }
+  }
