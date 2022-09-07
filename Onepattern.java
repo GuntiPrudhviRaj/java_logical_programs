fa6c5a1427e4d780ce8c100f29a1e182cacc1628
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-09-07 14:18:52 +0530

    Add files via upload

---
+++ Onepattern.java
@@ -0,0 +1,15 @@
+class Onepattern
+  {
+    public static void main(String args[])
+    {
+      int i,j;
+      for(i=1;i<=5;i++)
+        {
+          for(j=1;j<=5-i;j++)
+        {
+            System.out.print(j);    
+        }
+          System.out.println();
+        }
+    }
+  }
