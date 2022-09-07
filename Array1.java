7b6888765134b5e63bde5527f9bfdcce3fd2cdfd
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-09-07 21:49:35 +0530

    Add files via upload

---
+++ Array1.java
@@ -0,0 +1,12 @@
+class Array1
+  {
+    public static void main(String args[])
+    {
+      int a[]={10,20,30,40,50};
+      System.out.println("the a[0] value is:"+a[0]);
+     System.out.println("the a[1] value is:"+a[1]);
+      System.out.println("the a[2] value is:"+a[2]);
+      System.out.println("the a[3] value is:"+a[3]);
+      System.out.println("the a[4] value is:"+a[4]); 
+    }
+  }
