c2c64b3a91e49dc619c359b9500150dc2ba04023
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-09-07 14:19:12 +0530

    Add files via upload

---
+++ Twopattern.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Twopattern
+  {
+    public static void main(String args[])
+    {
+      int n=5,i,j;
+      Scanner sc=new Scanner(System.in);
+      for(i=1;i<=n;i++)
+        {
+          for(j=1;j<=n;j++)
+        {
+            System.out.print(j);    
+        }
+          System.out.println();
+        }
+    }
+  }
