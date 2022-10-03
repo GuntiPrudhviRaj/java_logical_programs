6cd6641b58478dea492f7b9f1b754d09618b34ee
GuntiPrudhviRaj <110899157+GuntiPrudhviRaj@users.noreply.github.com>
2022-10-03 17:52:14 +0530

    Add files via upload

---
+++ Readfile.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.lang.*;
+public class Readfile
+{
+public static void main(String args[])
+  {
+             try
+     {
+      File f=new File("Mainfile.txt");
+      FileInputStream fos=new FileInputStream(f);
+      int ch;
+      while( (ch=fos.read()) !=-1)
+       {
+        System.out.print((char)ch);
+                     Thread.sleep(100);
+                     }    
+     }
+              catch(IOException ie)
+      {
+       System.out.println(ie.getMessage());
+     }  
+              catch(InterruptedException iee)
+      {
+       System.out.println(iee.getMessage());
+     }  
+   }      
+  }
+  
+  
